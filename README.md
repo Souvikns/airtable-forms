23d5909a45b3d5ef14afd5680a55d13914f81081
Souvik <souvikde.ns@gmail.com>
2020-05-01 03:07:42 +0530

    adding env variables

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # airtable-forms
+[![DeepScan grade](https://deepscan.io/api/teams/8580/projects/11204/branches/164201/badge/grade.svg)](https://deepscan.io/dashboard#view=project&tid=8580&pid=11204&bid=164201)
+
+
+
  Makes collecting data from forms super easy
--- tests/test.js
+++ tests/test.js
@@ -1,4 +1,15 @@
+require('dotenv').config()
 
 test('fake test',()=>{
     expect(true).toBeTruthy()
 })
+
+function check(name){
+    return name
+}
+
+test('env test',()=>{
+    const n = check(process.env.API_KEY)
+    expect(n).toEqual(process.env.API_KEY)
+})
+

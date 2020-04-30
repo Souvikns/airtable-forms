eefb1c3560e60d7c07bc9385bf7d9366fc4cb789
Souvik <souvikde.ns@gmail.com>
2020-05-01 02:52:27 +0530

    making api

--- index.js
+++ index.js
@@ -5,5 +5,59 @@
 
 
 /**
- * 
+ * setup constant fields for apikey 
+ * setup constants feilds for base key  
  */
+
+const Airtable = require('airtable')
+const {API_KEY,BASE_KEY,BASE_NAME} = require('./config')
+
+class AirForms {
+    constructor({ api_key, base_key, base_name }) {
+        this.api_key = api_key
+        this.base_key = base_key
+        this.base_name = base_name
+
+        this.base = new Airtable({ apiKey: this.api_key }).base(this.base_key)
+
+    }
+
+    async listData({ max_record } = { max_record: null }) {
+
+        let data = []
+
+        if (max_record === null) {
+
+            await this.base(this.base_name).select({
+                view: "Grid view"
+            }).eachPage((records, fetchNextPage) => {
+
+                records.forEach(record => {
+                    data.push(record.fields)
+
+                })
+
+                fetchNextPage()
+            })
+
+        } else {
+            await this.base(this.base_name).select({
+                maxRecords: max_record,
+                view: "Grid view"
+            }).eachPage((records, fetchNextPage) => {
+
+                records.forEach(record => {
+                    data.push(record.fields)
+
+                })
+
+                fetchNextPage()
+            })
+        }
+
+        return data
+
+    }
+
+}
+
